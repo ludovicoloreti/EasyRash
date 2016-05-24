190e5267c55a92a028d99f29c0216fc1a1054c9b
Ludovico Loreti <ludovico.loreti@gmail.com>
2016-05-24 10:04:39 +0200

    Aggiornato server

--- app/routes.js
+++ app/routes.js
@@ -85,6 +85,7 @@ module.exports = function (app) {
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
+      // console.log(res)
         res.sendFile(__dirname + '/client/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
