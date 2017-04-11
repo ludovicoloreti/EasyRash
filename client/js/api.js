25804b20905ecf6cd22215cc08d4cd372906ec51
Ludovico Loreti <ludovico.loreti@gmail.com>
2017-04-11 16:29:24 +0200

    FINAL VERSION

---
+++ client/js/api.js
@@ -0,0 +1,98 @@
+angular.module('EasyRashApp.api', ['EasyRashApp.config'])
+
+.factory('Api', function($http,CONFIG,$window,$timeout,$q) {
+  var self = this;
+
+  self.getUsers = function() {
+    return $http.get(CONFIG.endpoint+CONFIG.users).then(
+      function(response) {
+        return response.data;
+      },function(error) {
+        return error;
+      });
+    };
+
+  self.getCurrentUser = function() {
+    return $http.get(CONFIG.endpoint + CONFIG.userinfo).then(function(result) {
+      return result.data;
+    },function(error) {
+      return error;
+    });
+  };
+
+  self.getEvents = function() {
+    return $http.get(CONFIG.endpoint+CONFIG.events).then(
+      function(response) {
+        return response.data;
+      },function(error) {
+        return error;
+      });
+    };
+
+  self.getEvent = function( eventId ) {
+    return $http.get(CONFIG.endpoint+CONFIG.event+"/"+eventId).then(
+      function(response) {
+        return response.data;
+      },function(error) {
+        return error;
+      });
+    };
+
+  self.getArticle = function( articleName, type ) {
+    if (type === "processed") {
+      return $http.get(CONFIG.endpoint+CONFIG.article+"/"+articleName).then(
+        function(response) {
+          return response.data;
+        },function(error) {
+          return error;
+        });
+    }else{
+      return $http.get(CONFIG.endpoint+CONFIG.raw_article+"/"+articleName).then(
+        function(response) {
+          return response.data;
+        },function(error) {
+          return error;
+        });
+      }
+    };
+
+  self.getArticlesToReview = function(){
+    return $http.get(CONFIG.endpoint+CONFIG.doclist).then(
+      function(response) {
+        return response.data;
+      },function(error) {
+        return error;
+      });
+    };
+
+  self.saveAnnotations = function( data ) {
+    return $http.post(CONFIG.endpoint+CONFIG.save_annotations+"/"+data.articleName, data).then(
+      function(response) {
+        return response.data;
+      },function(error) {
+        return error;
+      });
+    };
+
+    self.saveDecision = function( data ) {
+      return $http.post(CONFIG.endpoint+CONFIG.save_decision+"/"+data.articleName, data).then(
+        function(response) {
+          return response.data;
+        },function(error) {
+          return error;
+        });
+      };
+
+    self.updateUser = function( data ) {
+      return $http.post(CONFIG.endpoint+CONFIG.updateUser, data).then(
+        function(response) {
+          return response;
+        },function(error) {
+          return error;
+        });
+      };
+
+
+  return self;
+  }
+);
---
+++ client/js/interface-utils.js
@@ -0,0 +1,194 @@
+angular.module('EasyRashApp.interfaceUtils', [])
+
+.filter('cut', function () {
+  return function (value, wordwise, max, tail) {
+    if (!value) return '';
+
+    max = parseInt(max, 10);
+    if (!max) return value;
+    if (value.length <= max) return value;
+
+    value = value.substr(0, max);
+    if (wordwise) {
+      var lastspace = value.lastIndexOf(' ');
+      if (lastspace != -1) {
+        value = value.substr(0, lastspace);
+      }
+    }
+
+    return value + (tail || ' …');
+  };
+})
+.directive('scrollToItem', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            scrollTo: "@"
+        },
+        link: function(scope, $elm,attr) {
+
+            $elm.on('click', function() {
+                $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top-100 }, "slow");
+            });
+        }
+    }})
+.directive('starRating', function () {
+    return {
+        restrict: 'A',
+        template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+            '</li>' +
+            '</ul>',
+        scope: {
+            ratingValue: '=',
+            max: '=',
+            onRatingSelected: '&'
+        },
+        link: function (scope, elem, attrs) {
+
+            var updateStars = function () {
+                scope.stars = [];
+                for (var i = 0; i < scope.max; i++) {
+                    scope.stars.push({
+                        filled: i < scope.ratingValue
+                    });
+                }
+            };
+
+            scope.toggle = function (index) {
+                scope.ratingValue = index + 1;
+                scope.onRatingSelected({
+                    rating: index + 1
+                });
+            };
+
+            scope.$watch('ratingValue', function (oldVal, newVal) {
+                if (newVal) {
+                    updateStars();
+                }
+            });
+        }
+    }
+})
+.directive('particles', function($window) {
+  var config =   {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#feb131"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 100,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 100,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  };
+
+	return {
+		restrict: 'A',
+    replace: true,
+    template: '<div class="particleJs" id="particleJs"></div>',
+		link: function(scope, element, attrs, fn) {
+
+      $window.particlesJS('particleJs',config);
+    }
+	};
+});
